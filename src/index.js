5c20080377912b2795528586613c173f6e702506
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-17 15:39:30 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ const number = document.querySelector('span');
 
 number.innerText = 0;
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
 
 const countStore = createStore(countModifier);
@@ -24,10 +29,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: 'ADD' });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: 'MINUS' });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener('click', handleAdd);
